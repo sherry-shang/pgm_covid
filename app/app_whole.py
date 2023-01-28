cde0eadc6fd12a4b0655937a8f9c7b0188dbfb4c
sherry-shang <xiaoxiao.shang@mail.mcgill.ca>
2023-01-28 23:33:12 +0800

    ss

--- app/app_whole.py
+++ app/app_whole.py
@@ -2,7 +2,7 @@ import dash
 from dash import html, dcc, Dash
 import dash_bootstrap_components as dbc
 #external_stylesheets = 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-app = Dash(__name__,  external_stylesheets = [dbc.themes.BOOTSTRAP],routes_pathname_prefix='/app/', requests_pathname_prefix='/app/')
+app = Dash(__name__,  external_stylesheets = [dbc.themes.BOOTSTRAP],routes_pathname_prefix='/pgm/', requests_pathname_prefix='/pgm/')
 
 
 server = app.server
